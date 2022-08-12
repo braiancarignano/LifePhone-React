4233f1c9f5943dea49465b93d5d5616a04741411
Braian Carignano <braiancarignano@gmail.com>
2022-08-11 22:06:48 -0300

    Primer Entrega

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          LifePhone
+        </h1>
+        <h2>
+         Primer Proyecto en React
+        </h2>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://braiancarignano.github.io/lifephone/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Maquetado Anterior
         </a>
       </header>
     </div>
